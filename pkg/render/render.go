582f85c47994619d2752c93cc864cf5571180f1e
Tia Widi <tia.septianawidi@gmail.com>
2024-11-09 00:33:53 +0700

    Working with layout

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,13 +2,13 @@ package render
 
 import (
 	"fmt"
-	"net/http"
 	"html/template"
+	"net/http"
 )
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parasing template:", err)
